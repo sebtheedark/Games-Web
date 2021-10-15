4d3020afd29a3100e4c8a7e39038bb8c2d9c1b74
Kevin De La Torre <61712433+sebtheedark@users.noreply.github.com>
2021-10-14 23:50:53 -0700

    Add files via upload
    Added new section, added clickable buttons/links and attempted slideshow

--- scripts.js
+++ scripts.js
@@ -5,3 +5,24 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+//Script for slideshow
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 8000); // Change image every 8 seconds
+}
